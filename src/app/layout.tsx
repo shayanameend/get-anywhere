85e640804e83343d262016b8d2f157287b138ad0
Shayan Ameen <shayan.ameen.developer@gmail.com>
2023-09-04 13:53:20 +0500

    initial setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { Header, Footer } from "@/components";
+
+const inter = Inter({ subsets: ["latin"] });
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: Props) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
