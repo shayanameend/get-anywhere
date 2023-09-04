8fdcbb435018bc19915746062a228f0bab4b69f4
Shayan Ameen <shayan.ameen.developer@gmail.com>
2023-09-04 18:28:52 +0500

    next auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { Header, Footer } from "@/components";
+import { AuthProvider, Header, Footer } from "@/components";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,9 +12,11 @@ export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        <main>{children}</main>
-        <Footer />
+        <AuthProvider>
+          <Header />
+          <main>{children}</main>
+          <Footer />
+        </AuthProvider>
       </body>
     </html>
   );
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
-export { default as Footer } from "./Footer";
-export { default as Header } from "./Header";
+export { default as AuthProvider } from "@/components/AuthProvider";
+export { default as Footer } from "@/components/Footer";
+export { default as Header } from "@/components/Header";
