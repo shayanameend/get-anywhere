ff6366df05e1efedc13baad721a55f0409119f93
Shayan Ameen <shayan.ameen.developer@gmail.com>
2023-09-05 18:17:34 +0500

    stripe payment successful

--- src/lib/next-auth.ts
+++ src/lib/next-auth.ts
@@ -1,4 +1,4 @@
-import NextAuth, { AuthOptions } from "next-auth";
+import { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
